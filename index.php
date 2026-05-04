fe42ec9b499e08abcd01983ca778abc17b58cfe3
vinii64 <vicente.sutil@eest3necochea.edu.ar>
2026-05-04 16:51:01 -0300

    Creación de archivos, assets, styles y scripts.  Adicion de íconos

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cardinal Sur</title>
+    <link rel="stylesheet" href="styles/style.css">
+</head>
+<body>
+    
+<div class="navbar"> 
+
+
+  <a class="menu" href="">
+    <img src="assets/home.svg" class="icono"><p>INICIO</p>
+  </a>
+
+  <a class="menu" href="">
+    <img src="assets/image.svg" class="icono"> <p>FOTOS</p>
+  </a>
+
+  <a class="menu" href="">
+    <img src="assets/map.svg" class="icono"> <p>TRAVESÍAS</p>
+  </a>
+
+  <a class="menu" href="">
+    <img src="assets/help.svg" class="icono"> <p>ACERCA</p>
+  </a>
+            
+
+</div>
+
+
+<script src="script.js"></script>
+</body>
+</html>
